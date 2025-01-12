1cb575079514f9c98db531e668baeb193b112541
ChrstnRmpj <rempajo30113@gmail.com>
2025-01-12 13:24:04 +0800

    [ADDED] changes created last July

--- assets/js/cursor.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mousemove", function(event) {
-  var x = event.pageX;
-  var y = event.pageY;
-  
-  var cursor = document.querySelector(".custom-cursor");
-  cursor.style.display = "block";
-  cursor.style.transform = "translate(" + (x - 10) + "px, " + (y - 10) + "px)";
-});
